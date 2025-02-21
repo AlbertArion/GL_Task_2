461c52b265c222e768f06ff161bf945f4e25aeb0
Albert <albertarion@gmail.com>
2025-02-21 18:49:18 +0800

    [feature] Update UI details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 /*
  * @Author: Albert
  * @Date: 2025-02-20 12:35:42
- * @LastEditors: Albert
- * @LastEditTime: 2025-02-21 11:58:49
- * @FilePath: /workspace/GL_Task_2src/app/page.tsx
+ * @LastEditors: Elaine
+ * @LastEditTime: 2025-02-21 18:48:23
+ * @FilePath: /GL_Task_2/src/app/page.tsx
  * @Description: 
  * 
  */
@@ -21,32 +21,31 @@ const Home: NextPage = () => {
   return (
     <div className="flex justify-center w-full items-start flex-wrap  min-h-screen bg-gray-100 h-full">
       {/* 顶部导航栏 */}
-      <div className='bg-[yellow] w-full h-88 max-w-[1583px]' style={{ height: "907px", padding: '0px 20px', background: "linear-gradient(180deg, #121211 0%, #6E7069 100%)" }}>
-        <div style={{ display: "flex", flexDirection: "row", alignItems: "center", verticalAlign: "middle", background: '#F0E6D4', borderRadius: "32px", height: "64px", margin: '20px 40px' }}>
-          <span style={{ height: "32px", width: "2px", borderLeft: "2px solid #39280E", marginLeft: "30px" }} />
-          <span style={{ padding: '0px 20px 0px 6px', marginTop: "24px", height: '64px', lineHeight: "19px", textAlign: 'start', fontWeight: "bold", color: '#39280E' }}>Generation<br></br>Lab</span>
-          <span style={{ flex: 2 }}></span>
-          <span style={{ padding: '0px 20px', height: '64px', textAlign: 'center', lineHeight: '64px', color: '#47371F' }}>Science</span>
-          <span style={{ padding: '0px 20px', height: '64px', textAlign: 'center', lineHeight: '64px', color: '#47371F' }}>About US</span>
-          <span style={{ padding: '0px 20px', height: '64px', textAlign: 'center', lineHeight: '64px', color: '#47371F' }}>FAQs</span>
-          <span style={{ padding: '0px 20px', height: '64px', textAlign: 'center', lineHeight: '64px', color: '#47371F' }}>Blog</span>
-          <span style={{ flex: 1 }}></span>
-          <div style={{ display: 'inline', borderRadius: '20px', background: "#47371E", color: "white", fontSize: "16px", padding: "6px 24px" }}>For Providers</div>
-          <span style={{ flex: 0.3 }}></span>
-          <div style={{ display: 'inline', borderRadius: '20px', background: "#6C5127", color: "white", fontSize: "16px", padding: "6px 24px" }}>Order Test</div>
-          <div style={{ display: 'inline', fontSize: "16px", padding: "6px 24px", marginRight: "10px" }}>Sign in</div>
+      <div className='w-full h-[907px] max-w-[1583px] py-0 px-20 bg-frame14 bg-no-repeat bg-cover bg-center'>
+        <div className='flex flex-row items-center align-middle bg-[#F0E6D4] rounded-[32px] h-[64px] my-[20px] mx-[40px]'>
+          <img src="/images/LOGO-Blk.svg" className='ml-[30px]' alt="" srcset="" />
+          <span className='flex-[2_1_0%]'></span>
+          <span className='py-0 px-20 h-64 leading-[64px] text-center text-[#47371F]'>Science</span>
+          <span className='py-0 px-20 h-64 leading-[64px] text-center text-[#47371F]'>About US</span>
+          <span className='py-0 px-20 h-64 leading-[64px] text-center text-[#47371F]'>FAQs</span>
+          <span className='py-0 px-20 h-64 leading-[64px] text-center text-[#47371F]'>Blog</span>
+          <span className='flex-1'></span>
+          <div className='inline rounded-[20px] bg-[#47371E] text-white text-[16px] p-[6px_24px]'>For Providers</div>
+          <span className='flex-[0.3_1_0%]' ></span>
+          <div className='inline' style={{ borderRadius: '20px', background: "#6C5127", color: "white", fontSize: "16px", padding: "6px 24px" }}>Order Test</div>
+          <div className='inline' style={{  fontSize: "16px", padding: "6px 24px", marginRight: "10px" }}>Sign in</div>
         </div>
-        <div style={{ fontSize: "96px", fontWeight: "lighter", color: "white", marginLeft: "60px", marginTop: "60px" }}>Measure Aging</div>
-        <div style={{ fontSize: "52px", fontWeight: "lighter", fontStyle: "italic", color: "white", marginLeft: "60px", marginTop: "-10px" }}>widh golden standard</div>
+        <div style={{ fontSize: "96px",  color: "white", marginLeft: "60px", marginTop: "60px" }}>Measure Aging</div>
+        <div style={{ fontSize: "52px",  fontStyle: "italic", color: "white", marginLeft: "60px", marginTop: "-10px" }}>widh golden standard</div>
         <div style={{ marginLeft: "60px", marginTop: "30px" }}>
           <div style={{ width: "20px", backgroundImage: "url('/public/icon/arrow_left.svg')" }}></div>
-          <div style={{ display: "inline", padding: "24px", fontSize: "36px", color: "white", borderRadius: "40px", background: "#4F4E4D" }}>Learn about our test</div>
+          <div className='inline' style={{ padding: "24px", fontSize: "36px", color: "white", borderRadius: "40px", background: "#4F4E4D" }}>Learn about our test</div>
         </div>
-        <div style={{ display: "flex", flexDirection: "row", marginRight: "60px" }}>
+        <div className='flex' style={{ flexDirection: "row", marginRight: "60px" }}>
           <div style={{ flex: 1 }}></div>
           <div style={{ marginTop: "60px" }}>
-            <div style={{ fontSize: "96px", fontWeight: "lighter", color: "white" }}>Intervene Health</div>
-            <div style={{ fontSize: "52px", fontWeight: "lighter", fontStyle: "italic", color: "white", marginTop: "-10px", marginLeft: "120px" }}>widh scientific confidence</div>
+            <div style={{ fontSize: "96px",  color: "white" }}>Intervene Health</div>
+            <div style={{ fontSize: "52px",  fontStyle: "italic", color: "white", marginTop: "-10px", marginLeft: "120px" }}>widh scientific confidence</div>
             <div style={{ width: "auto", padding: "24px", marginTop: "30px", marginLeft: "320px", fontSize: "36px", color: "white", borderRadius: "40px", background: "#4F4E4D" }}>Learn about our plan</div>
           </div>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,4 @@
-/*
- * @Author: Albert
- * @Date: 2025-02-20 12:23:19
- * @LastEditors: Albert
- * @LastEditTime: 2025-02-21 12:09:40
- * @FilePath: /workspace/GL_Task_2tailwind.config.ts
- * @Description: 
- * 
- */
 import type { Config } from "tailwindcss";
-import frame14 from  './public/image/Frame14.jpg'
 
 export default {
   content: [
@@ -27,15 +17,15 @@ export default {
         foreground: "var(--foreground)",
       },
       backgroundImage:{
-        'entropy': "url('./public/image/ENTROPY CUREVE.svg')",
-        'frame14': "url('/public/image/Frame14.jpg')",
-        'frame26': "url('/public/image/Frame 2609340.svg')",
-        'image11': "url('/public/image/image 11.svg')",
-        'image13': "url('/public/image/image 13.svg')",
-        'image1': "url('/public/image/image1.svg')",
-        'image2': "url('/public/image/image2.svg')",
-        'image3': "url('/public/image/image3.svg')",
-        'LOGOBlk': "url('/public/image/LOGO-Blk.svg')"
+        'entropy': "url('/images/ENTROPY CUREVE.svg')",
+        'frame14': "url('/images/Frame14.jpg')",
+        'frame26': "url('/images/Frame 2609340.svg')",
+        'image11': "url('/images/image 11.svg')",
+        'image13': "url('/images/image 13.svg')",
+        'image1': "url('/images/image1.svg')",
+        'image2': "url('/images/image2.svg')",
+        'image3': "url('/images/image3.svg')",
+        'LOGOBlk': "url('/images/LOGO-Blk.svg')"
       }
     },
   },
